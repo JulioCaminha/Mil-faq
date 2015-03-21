7a9656ebf298e40f512e8647fdda0bc386f2cd9c
Julio Caminha <juliocaminha20@gmail.com>
2015-03-21 12:30:08 -0300

    #14 Adicionando metodo nome_completo

--- backend/milfaq/app/models/usuario.rb
+++ backend/milfaq/app/models/usuario.rb
@@ -1,5 +1,11 @@
 class Usuario < ActiveRecord::Base
 	belongs_to :perfil
-	validates :nome, :sobrenome, :email, :perfil_id, presence: true
+	validates :nome, :sobrenome, :email, presence: true
 	validates :email, uniqueness: true
+
+
+	def nome_completo
+		"#{nome} #{sobrenome}"
+	end
+
 end
